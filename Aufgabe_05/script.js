70663160766a1659da4b62f34c889b55962a5ea4
Sarah Mohring <sarah.erika.sigrid.mohring@hs-furtwangen.de>
2020-05-27 12:51:57 +0200

    1. Versuch Aufgabe 5

---
+++ Aufgabe_05/script.js
@@ -0,0 +1,43 @@
+"use strict";
+var Aufgabe_05;
+(function (Aufgabe_05) {
+    let divElemAll = document.createElement("div");
+    divElemAll.setAttribute("class", "flexProdukte");
+    for (let index = 0; index < Aufgabe_05.produkteKamera.length; index++) {
+        let divElem = document.createElement("div");
+        divElem.setAttribute("class", "kameras");
+        divElemAll.appendChild(divElem);
+        let imgElem = document.createElement("img");
+        imgElem.setAttribute("src", Aufgabe_05.produkteKamera[index].imgurl);
+        divElem.appendChild(imgElem);
+        let p1Elem = document.createElement("p");
+        divElem.appendChild(p1Elem);
+        p1Elem.innerHTML = Aufgabe_05.produkteKamera[index].name;
+        let p2Elem = document.createElement("p");
+        divElem.appendChild(p2Elem);
+        p2Elem.innerHTML = Aufgabe_05.produkteKamera[index].beschreibung;
+        let p3Elem = document.createElement("p");
+        divElem.appendChild(p3Elem);
+        p3Elem.innerHTML = Aufgabe_05.produkteKamera[index].preis;
+    }
+    for (let index = 0; index < Aufgabe_05.produkteZubehoer.length; index++) {
+        let divElem = document.createElement("div");
+        divElem.setAttribute("class", "zubehoer"); //?????
+        divElemAll.appendChild(divElem);
+        let imgElem = document.createElement("img");
+        imgElem.setAttribute("src", Aufgabe_05.produkteZubehoer[index].imgurl);
+        divElem.appendChild(imgElem);
+        let p1Elem = document.createElement("p");
+        divElem.appendChild(p1Elem);
+        p1Elem.innerHTML = Aufgabe_05.produkteZubehoer[index].beschreibung;
+        let p2Elem = document.createElement("p");
+        divElem.appendChild(p2Elem);
+        p2Elem.innerHTML = Aufgabe_05.produkteZubehoer[index].beschreibung;
+        let p3Elem = document.createElement("p");
+        divElem.appendChild(p3Elem);
+        //  p3Elem.setAttribute("class", "preis");
+        p3Elem.innerHTML = Aufgabe_05.produkteZubehoer[index].preis;
+    }
+    document.getElementById("main")?.appendChild(divElemAll);
+})(Aufgabe_05 || (Aufgabe_05 = {}));
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe_05/script.ts
@@ -0,0 +1,55 @@
+namespace Aufgabe_05 {
+
+    let divElemAll: HTMLElement = document.createElement("div");
+    divElemAll.setAttribute("class", "flexProdukte");
+
+    for (let index: number = 0; index < produkteKamera.length; index++) {
+
+        let divElem: HTMLElement = document.createElement("div");
+        divElem.setAttribute("class", "kameras");
+        divElemAll.appendChild(divElem);
+
+        let imgElem: HTMLElement = document.createElement("img");
+        imgElem.setAttribute("src", produkteKamera[index].imgurl);
+        divElem.appendChild(imgElem);
+
+        let p1Elem: HTMLElement = document.createElement("p");
+        divElem.appendChild(p1Elem);
+        p1Elem.innerHTML = produkteKamera[index].name;
+
+        let p2Elem: HTMLElement = document.createElement("p");
+        divElem.appendChild(p2Elem);
+        p2Elem.innerHTML = produkteKamera[index].beschreibung;
+
+        let p3Elem: HTMLElement = document.createElement("p");
+        divElem.appendChild(p3Elem);
+        p3Elem.innerHTML = produkteKamera[index].preis;
+    }
+
+    for (let index: number = 0; index < produkteZubehoer.length; index++) {
+
+        let divElem: HTMLElement = document.createElement("div");
+        divElem.setAttribute("class", "zubehoer");                  //?????
+        divElemAll.appendChild(divElem);
+
+        let imgElem: HTMLElement = document.createElement("img");
+        imgElem.setAttribute("src", produkteZubehoer[index].imgurl);
+        divElem.appendChild(imgElem);
+
+        let p1Elem: HTMLElement = document.createElement("p");
+        divElem.appendChild(p1Elem);
+        p1Elem.innerHTML = produkteZubehoer[index].beschreibung;
+
+        let p2Elem: HTMLElement = document.createElement("p");
+        divElem.appendChild(p2Elem);
+        p2Elem.innerHTML = produkteZubehoer[index].beschreibung;
+
+        let p3Elem: HTMLElement = document.createElement("p");
+        divElem.appendChild(p3Elem);
+    //  p3Elem.setAttribute("class", "preis");
+        p3Elem.innerHTML = produkteZubehoer[index].preis;
+    }
+
+    document.getElementById("main")?.appendChild(divElemAll);
+
+}
