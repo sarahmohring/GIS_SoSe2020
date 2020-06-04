432ac462265862c01e8b51c3fbae81d624523def
Sarah Mohring <sarah.erika.sigrid.mohring@hs-furtwangen.de>
2020-06-04 12:54:17 +0200

    Teilaufgabe 1

--- Aufgabe_06/script.js
+++ Aufgabe_06/script.js
@@ -1,56 +1,73 @@
 "use strict";
 var Aufgabe_06;
 (function (Aufgabe_06) {
-    // großes Div-Element erstellen, dass alle zu generierenden Artikel umfassen wird
+    let zaehler = []; // Variable fuer Produktzaehlerfunktion 
+    // großes Div-Element, wird alle Kamera-Artikel umfassen
     let kategorie = document.createElement("div");
     kategorie.setAttribute("id", "kamerasDiv");
-    function produkteErzeugen(_produktliste, produktDiv) {
+    function produkteErzeugen(_produktliste, _produktDiv) {
         // Schleife generiert alle Produkte aus der scriptProduktdaten.ts-Datei
         for (let index = 0; index < _produktliste.length; index++) {
             // umgebender Div-Block pro Produkt
             let divProdukt = document.createElement("div");
-            divProdukt.setAttribute("class", "produkte"); //??
-            produktDiv.appendChild(divProdukt);
-            // Bild einfuegen
+            divProdukt.setAttribute("class", "produkte");
+            _produktDiv.appendChild(divProdukt);
+            // Bild
             let imgURL = document.createElement("img");
             imgURL.setAttribute("src", _produktliste[index].imgurl);
             imgURL.setAttribute("alt", _produktliste[index].name);
             divProdukt.appendChild(imgURL);
-            // Produktname einfuegen
+            // Produktname
             let h3Name = document.createElement("h3");
             h3Name.innerHTML = _produktliste[index].name;
             divProdukt.appendChild(h3Name);
-            // Beschreibung einfuegen
+            // Beschreibung
             let pBeschr = document.createElement("p");
             pBeschr.innerHTML = _produktliste[index].beschreibung;
             divProdukt.appendChild(pBeschr);
-            // Preis einfuegen
+            // Preis
             let pPreis = document.createElement("p");
-            pPreis.setAttribute("class", "preis");
-            pPreis.innerHTML = "Preis: " + _produktliste[index].preis + "€";
+            pPreis.innerHTML = "Preis: " + _produktliste[index].preis.toLocaleString("de-DE", { "currency": "EUR", "style": "currency" });
             divProdukt.appendChild(pPreis);
-            // Warenkorb-Button einfuegen
+            // #region Warenkorb-Button + Funktion 
             let warenkorb = document.createElement("button");
-            warenkorb.setAttribute("type", "button");
-            warenkorb.setAttribute("class", "knopf");
-            divProdukt.appendChild(warenkorb);
             warenkorb.innerHTML = "In den Warenkorb!";
-            //warenkorb.addEventListener("click", handleClick);
+            divProdukt.appendChild(warenkorb);
+            warenkorb.addEventListener("click", handleClick);
+            function handleClick() {
+                document.getElementById("zahl")?.remove();
+                let zaehlerDiv = document.createElement("div"); // Div fuer kleinen Kreis am Warenkorb-Icon
+                zaehlerDiv.setAttribute("id", "zaehler");
+                document.getElementById("kreisIcon")?.appendChild(zaehlerDiv);
+                let zahlIcon = document.createElement("p"); // Zahl im Kreis
+                zahlIcon.setAttribute("id", "zahl");
+                zaehlerDiv.appendChild(zahlIcon);
+                zaehler.push(_produktliste[index].preis);
+                zahlIcon.innerHTML = "" + zaehler.length; // Zahl der Produkte im Warenkorb erhoehen bei Klick
+                let summe = 0;
+                for (let index = 0; index < zaehler.length; index++) { // Preis der neuen Produkte zum bisherigen Preis addieren
+                    summe = summe + zaehler[index];
+                }
+                console.log("Summe:" + summe.toLocaleString("de-DE", { "currency": "EUR", "style": "currency" })); // Gesamtpreis in Konsole ausgeben
+                // #endregion
+            }
         }
     }
-    produkteErzeugen(Aufgabe_06.produkteKamera, kategorie);
-    document.getElementById("kameras")?.appendChild(kategorie);
-    let kategorie2 = document.createElement("div");
+    // #region Produkte erzeugen und je nach Klickverhalten anzeigen/verstecken
+    produkteErzeugen(Aufgabe_06.produkteKamera, kategorie); // Kameraprodukte erzeugen und... 
+    document.getElementById("kameras")?.appendChild(kategorie); // ...an am Anfang deklariertes Div anhaengen
+    let kategorie2 = document.createElement("div"); // Div fuer Zubehoer erzeugen
     kategorie2.setAttribute("id", "zubehoerDiv");
-    produkteErzeugen(Aufgabe_06.produkteZubehoer, kategorie2);
-    document.getElementById("zubehoer")?.appendChild(kategorie2);
+    produkteErzeugen(Aufgabe_06.produkteZubehoer, kategorie2); // Zubehoerprodukte erzeugen
+    document.getElementById("zubehoer")?.appendChild(kategorie2); // an Zubehoer-Div anhaengen 
+    // Eventlistener fuer Navigation - Filter nach Kategorie
     let nurKameras = document.getElementById("nurKameras");
     nurKameras.addEventListener("click", handleKameras);
     let nurZubehoer = document.getElementById("nurZubehoer");
     nurZubehoer.addEventListener("click", handleZubehoer);
     let alleProdukte = document.getElementById("alleProdukte");
     alleProdukte.addEventListener("click", handleAlleProdukte);
-    //Funktionen für Filter
+    // Funktionen, um nach den 3 Kategorien zu filtern
     function handleKameras() {
         let kamerasDiv = document.getElementById("kameras"); //tag doppelt?
         kamerasDiv.hidden = false;
@@ -81,5 +98,6 @@ var Aufgabe_06;
         let titelZubehoer = document.getElementById("titel2");
         titelZubehoer.hidden = false;
     }
+    // #endregion
 })(Aufgabe_06 || (Aufgabe_06 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe_06/script.ts
+++ Aufgabe_06/script.ts
@@ -1,64 +1,89 @@
 namespace Aufgabe_06 {
 
-    // großes Div-Element erstellen, dass alle zu generierenden Artikel umfassen wird
+    let zaehler: number[] = []; // Variable fuer Produktzaehlerfunktion 
+
+    // großes Div-Element, wird alle Kamera-Artikel umfassen
     let kategorie: HTMLDivElement = document.createElement("div");
     kategorie.setAttribute("id", "kamerasDiv");
 
-    function produkteErzeugen(_produktliste: Produkt[], produktDiv: HTMLElement): void {
+    function produkteErzeugen(_produktliste: Produkt[], _produktDiv: HTMLElement): void {
 
         // Schleife generiert alle Produkte aus der scriptProduktdaten.ts-Datei
         for (let index: number = 0; index < _produktliste.length; index++) {
 
             // umgebender Div-Block pro Produkt
             let divProdukt: HTMLDivElement = document.createElement("div");
-            divProdukt.setAttribute("class", "produkte"); //??
-            produktDiv.appendChild(divProdukt);
+            divProdukt.setAttribute("class", "produkte");
+            _produktDiv.appendChild(divProdukt);
 
-            // Bild einfuegen
+            // Bild
             let imgURL: HTMLImageElement = document.createElement("img");
             imgURL.setAttribute("src", _produktliste[index].imgurl);
             imgURL.setAttribute("alt", _produktliste[index].name);
             divProdukt.appendChild(imgURL);
 
-            // Produktname einfuegen
+            // Produktname
             let h3Name: HTMLParagraphElement = document.createElement("h3");
             h3Name.innerHTML = _produktliste[index].name;
             divProdukt.appendChild(h3Name);
 
-            // Beschreibung einfuegen
+            // Beschreibung
             let pBeschr: HTMLParagraphElement = document.createElement("p");
             pBeschr.innerHTML = _produktliste[index].beschreibung;
             divProdukt.appendChild(pBeschr);
 
-            // Preis einfuegen
+            // Preis
             let pPreis: HTMLParagraphElement = document.createElement("p");
-            pPreis.setAttribute("class", "preis");
-            pPreis.innerHTML = "Preis: " + _produktliste[index].preis + "€";
+            pPreis.innerHTML = "Preis: " + _produktliste[index].preis.toLocaleString("de-DE", { "currency": "EUR", "style": "currency" });
             divProdukt.appendChild(pPreis);
 
-            // Warenkorb-Button einfuegen
+            // #region Warenkorb-Button + Funktion 
             let warenkorb: HTMLButtonElement = document.createElement("button");
-            warenkorb.setAttribute("type", "button");
-            warenkorb.setAttribute("class", "knopf");
-            divProdukt.appendChild(warenkorb);
             warenkorb.innerHTML = "In den Warenkorb!";
-            //warenkorb.addEventListener("click", handleClick);
+            divProdukt.appendChild(warenkorb);
+            
+            warenkorb.addEventListener("click", handleClick);
+
+            function handleClick(): void { // wird aufgerufen bei Klick auf "In den Warenkorb!"
+                document.getElementById("zahl")?.remove();
+
+                let zaehlerDiv: HTMLElement = document.createElement("div"); // Div fuer kleinen Kreis am Warenkorb-Icon
+                zaehlerDiv.setAttribute("id", "zaehler");
+                document.getElementById("kreisIcon")?.appendChild(zaehlerDiv);
+
+                let zahlIcon: HTMLElement = document.createElement("p"); // Zahl im Kreis
+                zahlIcon.setAttribute("id", "zahl");
+                zaehlerDiv.appendChild(zahlIcon);
+
+                zaehler.push(_produktliste[index].preis);
+
+                zahlIcon.innerHTML = "" + zaehler.length; // Zahl der Produkte im Warenkorb erhoehen bei Klick
+
+                let summe: number = 0;
+                for (let index: number = 0; index < zaehler.length; index++) { // Preis der neuen Produkte zum bisherigen Preis addieren
+                    summe = summe + zaehler[index];
+                }
+
+                console.log("Summe:" + summe.toLocaleString("de-DE", { "currency": "EUR", "style": "currency" })); // Gesamtpreis in Konsole ausgeben
+                // #endregion
+            }
         }
 
     }
 
-    produkteErzeugen(produkteKamera, kategorie);
+    // #region Produkte erzeugen und je nach Klickverhalten anzeigen/verstecken
+    produkteErzeugen(produkteKamera, kategorie); // Kameraprodukte erzeugen und... 
 
-    document.getElementById("kameras")?.appendChild(kategorie);
+    document.getElementById("kameras")?.appendChild(kategorie); // ...an am Anfang deklariertes Div anhaengen
 
-    let kategorie2: HTMLElement = document.createElement("div");
+    let kategorie2: HTMLElement = document.createElement("div"); // Div fuer Zubehoer erzeugen
     kategorie2.setAttribute("id", "zubehoerDiv");
 
-    produkteErzeugen(produkteZubehoer, kategorie2);
+    produkteErzeugen(produkteZubehoer, kategorie2); // Zubehoerprodukte erzeugen
 
-    document.getElementById("zubehoer")?.appendChild(kategorie2);
+    document.getElementById("zubehoer")?.appendChild(kategorie2); // an Zubehoer-Div anhaengen 
 
-
+    // Eventlistener fuer Navigation - Filter nach Kategorie
     let nurKameras: HTMLElement = <HTMLElement>document.getElementById("nurKameras");
     nurKameras.addEventListener("click", handleKameras);
 
@@ -68,16 +93,15 @@ namespace Aufgabe_06 {
     let alleProdukte: HTMLElement = <HTMLElement>document.getElementById("alleProdukte");
     alleProdukte.addEventListener("click", handleAlleProdukte);
 
-    //Funktionen für Filter
-
-    function handleKameras(): void {
+    // Funktionen, um nach den 3 Kategorien zu filtern
+    function handleKameras(): void { // Klick im Menu auf "Kameras" - Titel und Produkte Zubehoer verstecken
 
         let kamerasDiv: HTMLElement = <HTMLElement>document.getElementById("kameras"); //tag doppelt?
         kamerasDiv.hidden = false;
 
         let titelKamera: HTMLElement = <HTMLElement>document.getElementById("titel1");
         titelKamera.hidden = false;
-        
+
         let zubehoerDiv: HTMLElement = <HTMLElement>document.getElementById("zubehoer"); //zubehoer bereits verwendet??
         zubehoerDiv.hidden = true;
 
@@ -85,7 +109,7 @@ namespace Aufgabe_06 {
         titelZubehoer.hidden = true;
     }
 
-    function handleZubehoer(): void {
+    function handleZubehoer(): void { // Klick im Menu auf "Zubehoer" - Titel und Produkte Kamera verstecken
 
         let kamerasDiv: HTMLElement = <HTMLElement>document.getElementById("kameras");
         kamerasDiv.hidden = true;
@@ -100,7 +124,7 @@ namespace Aufgabe_06 {
         titelZubehoer.hidden = false;
     }
 
-    function handleAlleProdukte(): void {
+    function handleAlleProdukte(): void { // Klick im Menu auf "Alle Artikel" - nichts verstecken
 
         let kamerasDiv: HTMLElement = <HTMLElement>document.getElementById("kameras");
         kamerasDiv.hidden = false;
@@ -114,4 +138,5 @@ namespace Aufgabe_06 {
         let titelZubehoer: HTMLElement = <HTMLElement>document.getElementById("titel2");
         titelZubehoer.hidden = false;
     }
+    // #endregion
 }
