6aa92660f01996c38f11681b31880a286a491b2c
Sarah Mohring <sarah.erika.sigrid.mohring@hs-furtwangen.de>
2020-06-17 15:33:28 +0200

    neuer Versuch

--- Aufgabe_08/script.js
+++ Aufgabe_08/script.js
@@ -1,22 +1,40 @@
 "use strict";
 var Aufgabe_08;
 (function (Aufgabe_08) {
-    document.getElementById("button")?.addEventListener("click", buttonHandler);
-    let formData = new FormData(document.forms[0]);
-    async function zuURL() {
-        let url = "https://gis-sose2020.herokuapp.com/";
-        let query = new URLSearchParams(formData);
+    /*document.getElementById("button")?.addEventListener("click", buttonHandler);
+    let formData: FormData = new FormData(document.forms[0]);
+
+    async function zuURL(): Promise<void> {
+        let url: string = "https://gis-sose2020.herokuapp.com/";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += url + "?" + query.toString();
         console.log(url);
-        return url;
+        
     }
-    async function buttonHandler() {
-        responseHolen(await zuURL());
+
+    async function buttonHandler(): Promise<void> {
+        responseHolen("https://gis-sose2020.herokuapp.com/");
+    }
+
+    async function responseHolen(_url: RequestInfo): Promise<void> {
+        let response1: Response = await fetch(_url);
+        let response2: string = await response1.text();
+        console.log("Response", response2);
+    }
+}*/
+    let formData;
+    document.getElementById("button")?.addEventListener("click", buttonHandler);
+    function buttonHandler() {
+        responseHolen("https://gis-sose2020.herokuapp.com/");
     }
     async function responseHolen(_url) {
-        let response1 = await fetch(_url);
-        let response2 = await response1.text();
-        console.log("Response", response2);
+        formData = new FormData(document.forms[0]);
+        let url = "" + _url;
+        let query = new URLSearchParams(formData);
+        url += url + "?" + query.toString();
+        let response = await fetch(url);
+        let responseToUser = await response.text();
+        console.log(responseToUser);
     }
 })(Aufgabe_08 || (Aufgabe_08 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe_08/script.ts
+++ Aufgabe_08/script.ts
@@ -1,18 +1,18 @@
 namespace Aufgabe_08 {
 
-    document.getElementById("button")?.addEventListener("click", buttonHandler);
+    /*document.getElementById("button")?.addEventListener("click", buttonHandler);
     let formData: FormData = new FormData(document.forms[0]);
 
-    async function zuURL(): Promise<string> {
+    async function zuURL(): Promise<void> {
         let url: string = "https://gis-sose2020.herokuapp.com/";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url += url + "?" + query.toString();
         console.log(url);
-        return url;
+        
     }
 
     async function buttonHandler(): Promise<void> {
-        responseHolen(await zuURL());
+        responseHolen("https://gis-sose2020.herokuapp.com/");
     }
 
     async function responseHolen(_url: RequestInfo): Promise<void> {
@@ -20,4 +20,21 @@ namespace Aufgabe_08 {
         let response2: string = await response1.text();
         console.log("Response", response2);
     }
-}
+}*/
+    let formData: FormData;
+    document.getElementById("button")?.addEventListener("click", buttonHandler);
+
+    function buttonHandler(): void {
+        responseHolen("https://gis-sose2020.herokuapp.com/");
+    }
+
+    async function responseHolen(_url: RequestInfo): Promise<void> {
+        formData = new FormData(document.forms[0]);
+        let url: string = "" + _url;
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url += url + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let responseToUser: String = await response.text();
+        console.log(responseToUser);
+    }
+} 
