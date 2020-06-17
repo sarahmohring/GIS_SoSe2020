f58d432be9086e36f8efb3ea1cec5c28175e9424
Sarah Mohring <sarah.erika.sigrid.mohring@hs-furtwangen.de>
2020-06-17 15:40:00 +0200

    ok sollte klappen

--- Aufgabe_08/script.js
+++ Aufgabe_08/script.js
@@ -1,27 +1,6 @@
 "use strict";
 var Aufgabe_08;
 (function (Aufgabe_08) {
-    /*document.getElementById("button")?.addEventListener("click", buttonHandler);
-    let formData: FormData = new FormData(document.forms[0]);
-
-    async function zuURL(): Promise<void> {
-        let url: string = "https://gis-sose2020.herokuapp.com/";
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url += url + "?" + query.toString();
-        console.log(url);
-        
-    }
-
-    async function buttonHandler(): Promise<void> {
-        responseHolen("https://gis-sose2020.herokuapp.com/");
-    }
-
-    async function responseHolen(_url: RequestInfo): Promise<void> {
-        let response1: Response = await fetch(_url);
-        let response2: string = await response1.text();
-        console.log("Response", response2);
-    }
-}*/
     let formData;
     document.getElementById("button")?.addEventListener("click", buttonHandler);
     function buttonHandler() {
